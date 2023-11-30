5c2319ad6b47e72b9b70ca7175d442b2e22a44af
Mehmet Nadi <mhmtalperennadi@gmail.com>
2023-11-30 14:17:28 -0800

    feat: play sound with gestrues

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,14 +7,39 @@ import { PredictionEvent } from '../prediction-event';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  gesture: String = "";
-  constructor() { }
+  gesture: string = "";
+    gestureSound: Map<string, string> = new Map(); // Initialize as a new Map and fix typo
+
+  constructor() { 
+    this.gestureSound.set('Open Hand', "snare.mp3");
+    this.gestureSound.set('Closed Hand', "kick-bass.mp3");
+    this.gestureSound.set('Hand Pointing', "crash.mp3");
+    this.gestureSound.set('Two Open Hands', "tom-1.mp3");
+    this.gestureSound.set('Two Closed Hands', "tom-2.mp3");
+    this.gestureSound.set('Hand Pinching', "tom-3.mp3");
+    this.gestureSound.set('Two Hands Pointing', "tom-4.mp3");
+  }
 
   ngOnInit(): void {
   }
 
-  prediction(event: PredictionEvent){
+  prediction(event: PredictionEvent) {
     this.gesture = event.getPrediction();
+    const soundName = this.gestureSound.get(this.gesture);
+  
+    if (soundName) { 
+      this.playSound(soundName);
+    } else {
+      console.error('No sound found for gesture:', this.gesture);
+    }
   }
+  
+  playSound(soundName: string) {
+    const soundPath = `../../assets/sounds/${soundName}`;
+    
+    const sound = new Audio(soundPath);
+    sound.play().catch(e => console.error('Error playing sound:', e));
+  }
+  
 
 }
