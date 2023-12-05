34eec6eb6492ddfb13e9e9879ada366c215e22e4
Levi Ramirez <levi.ramirez135@gmail.com>
2023-12-04 19:40:19 -0800

    feat: colors changing per activated instrument

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -19,10 +19,52 @@ export class HomePageComponent implements OnInit {
     this.gestureSound.set('Hand Pinching', "tom-3.mp3");
     this.gestureSound.set('Two Hands Pointing', "tom-4.mp3");
   }
+  //have a function for each one (kinda hard codded) and return a truth value for if its on or not
+  openHand(): string{
+    if(this.gesture == 'Open Hand') {
+      return 'Yellow'
+    }
+    return 'Wheat'
+  }
+  closedHand(): string{
+    if(this.gesture == 'Closed Hand') {
+      return 'Yellow'
+    }
+    return 'Wheat'
+  }
+  pointingHand(): string{
+    if(this.gesture == 'Hand Pointing') {
+      return 'Yellow'
+    }
+    return 'Wheat'
+  }
+  twoOpenHand(): string{
+    if(this.gesture == 'Two Open Hands') {
+      return 'Yellow'
+    }
+    return 'Wheat'
+  }
+  twoClosedHand(): string{
+    if(this.gesture == 'Two Closed Hands') {
+      return 'Yellow'
+    }
+    return 'Wheat'
+  }
+  pinchingHand(): string{
+    if(this.gesture == 'Hand Pinching') {
+      return 'Yellow'
+    }
+    return 'Wheat'
+  }
+  twoPointingHand(): string{
+    if(this.gesture == 'Two Hands Pointing') {
+      return 'Yellow'
+    }
+    return 'Wheat'
+  }
 
   ngOnInit(): void {
   }
-
   prediction(event: PredictionEvent) {
     this.gesture = event.getPrediction();
     const soundName = this.gestureSound.get(this.gesture);
@@ -40,6 +82,8 @@ export class HomePageComponent implements OnInit {
     const sound = new Audio(soundPath);
     sound.play().catch(e => console.error('Error playing sound:', e));
   }
+
+
   
 
 }
